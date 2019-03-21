c35f99f63f488d20f17fa3fa8da193a1aa6c7e10
Sorin Chis <kish.sorin@yahoo.com>
2019-03-21 15:41:42 +0200

    succesfully imported the project from yesterday and created server.js file for heroku deployment

---
+++ build/precache-manifest.4e0d573e2c10ab9d108962b87f80ea0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3f4cc5178d5cd61e95df",
+    "url": "/static/js/main.db5ee13b.chunk.js"
+  },
+  {
+    "revision": "b37a143a263298450f99",
+    "url": "/static/js/2.3d371ace.chunk.js"
+  },
+  {
+    "revision": "3f4cc5178d5cd61e95df",
+    "url": "/static/css/main.e6624447.chunk.css"
+  },
+  {
+    "revision": "017d3023d9c8a3d393c7503c342addd6",
+    "url": "/index.html"
+  }
+];
